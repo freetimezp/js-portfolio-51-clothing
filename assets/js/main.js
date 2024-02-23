c32d4aa9de604437becf35bcf67e1090ad24ac11
freetimezp <kraftukrnet@ukr.net>
2024-02-23 16:02:19 +0200

    06. js-portfolio-51-clothing. Shop section. Filter products.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,9 @@ const navBar = document.querySelector(".header");
 const navBtn = document.querySelector(".header__btn");
 const circleBtn = document.querySelector(".go-down-btn");
 const newContent = document.querySelector(".new__products");
+const shopContent = document.querySelector(".shop__products");
+const shopCategories = document.querySelectorAll(".shop__category");
+
 
 //toggle menu
 navBtn.addEventListener('click', () => {
@@ -45,7 +48,7 @@ homeSwiper.on("slideChangeTransitionEnd", () => {
 
 
 //new products API
-async function renderShopProducts() {
+async function renderNewProducts() {
     const response = await fetch(API_URL);
     const data = await response.json();
 
@@ -97,9 +100,50 @@ async function renderShopProducts() {
     });
 }
 
+//shop products API
+async function renderShopProducts() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    data.map((product) => {
+        shopContent.innerHTML += ProductCard(product);
+    });
+
+    const productCards = shopContent.querySelectorAll(".product-card");
+    productCards.forEach((product) => {
+        product.classList.add("shop__product");
+        const image = product.querySelector("img");
+        product.addEventListener("mouseover", () => {
+            if (product.dataset.image2 !== "undefined") {
+                image.src = product.dataset.image2;
+            }
+        });
+        product.addEventListener("mouseleave", () => {
+            image.src = product.dataset.image1;
+        });
+    });
+}
+//shop categories
+shopCategories.forEach((category) => {
+    category.addEventListener('click', () => {
+        shopCategories.forEach((category) => category.classList.remove("selected"));
+        category.classList.add("selected");
+
+        let categoryType = category.dataset.category;
+        const shopProducts = document.querySelectorAll(".shop__product");
+        shopProducts.forEach((product) => {
+            product.classList.add("hide");
+            if (product.dataset.category === categoryType || categoryType === "all") {
+                product.classList.remove('hide');
+            }
+        });
+    });
+});
+
 
 //onload
 window.addEventListener("load", () => {
+    renderNewProducts();
     renderShopProducts();
     document.querySelector(".home__slide").classList.add("reveal");
 });
