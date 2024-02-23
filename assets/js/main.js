28ea7a085c45e4c760f4b93b50b0e6af8ea1734b
freetimezp <kraftukrnet@ukr.net>
2024-02-23 19:45:09 +0200

    11. js-portfolio-51-clothing. Scrollup btn. Change header bg on scroll. Set active link on scroll. ScrollReveal animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,24 @@ const newContent = document.querySelector(".new__products");
 const shopContent = document.querySelector(".shop__products");
 const shopCategories = document.querySelectorAll(".shop__category");
 const trendingContent = document.querySelector(".trending__products");
+const scrollUpBtn = document.querySelector(".scroll-up");
+const sections = document.querySelectorAll("section[id]");
 
 
 //toggle menu
 navBtn.addEventListener('click', () => {
     document.body.classList.toggle("menu-toggled");
 });
+
+//change header bg
+function changeHeaderBg() {
+    const scrollY = window.scrollY;
+    if (scrollY > 100) {
+        navBar.style.backgroundColor = 'var(--white-100-opcty-212)';
+    } else {
+        navBar.style.backgroundColor = 'transparent';
+    }
+}
 
 //circle text
 let circleText = circleBtn.querySelector(".circle-text");
@@ -25,6 +37,17 @@ circleText.innerHTML = circleText.textContent
     .map((char, index) => `<span style="transform: rotate(${index * 28.3}deg)">${char}</span>`)
     .join("");
 
+
+//scrollreveal animation
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "100px",
+    duration: 2000,
+    delay: 300
+});
+sr.reveal(".brands__logo", { interval: 100 });
+sr.reveal(".subscribe", { scale: 0.75 });
+sr.reveal(".footer__col", { interval: 100 });
 
 //cwiper slider
 const homeSwiper = new Swiper(".home__content", {
@@ -100,6 +123,9 @@ async function renderNewProducts() {
             },
         }
     });
+
+    //scroll reveal animation
+    sr.reveal(newContent);
 }
 
 //shop products API
@@ -124,7 +150,11 @@ async function renderShopProducts() {
             image.src = product.dataset.image1;
         });
     });
+
+    //scroll reveal animation
+    sr.reveal(".shop__product", { interval: 100 });
 }
+
 //shop categories
 shopCategories.forEach((category) => {
     category.addEventListener('click', () => {
@@ -164,8 +194,42 @@ async function renderTrendingProducts() {
         }
     });
 
+    //scroll reveal animation
+    sr.reveal(trendingContent);
 }
 
+//scroll up button
+function showScrollUpBtn() {
+    if (window.scrollY > 300) {
+        scrollUpBtn.classList.add("show");
+    } else {
+        scrollUpBtn.classList.remove("show");
+    }
+}
+scrollUpBtn.addEventListener("click", () => window.scrollTo({ behavior: "smooth", top: 0, left: 0 }));
+
+//active on scroll
+function activeScroll() {
+    const scrollY = window.scrollY;
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 16,
+            sectionHeight = section.offsetHeight,
+            link = document.querySelector(`.header__link a[href='#${section.id}'`);
+        if (scrollY >= sectionTop && scrollY <= sectionHeight + sectionTop) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+}
+
+
+//onscroll
+window.addEventListener("scroll", () => {
+    activeScroll();
+    changeHeaderBg();
+    showScrollUpBtn();
+});
 
 //onload
 window.addEventListener("load", () => {
@@ -174,6 +238,9 @@ window.addEventListener("load", () => {
     renderTrendingProducts();
     document.querySelector(".home__slide").classList.add("reveal");
 });
+
+
+
 
 
 
